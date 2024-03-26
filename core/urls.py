96dccfc7c2c0c6ad0dbbe5717c3c069520db67c7
Tupesh <tupesghimire@gmail.com>
2024-03-26 21:30:13 +0545

    all html page layout added

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about', views.about, name='about'),
+    path('bookingForm', views.bookingForm, name='bookingForm'),
+    path('donationCentres', views.donationCentres, name='donationCentres'),
+    path('donationProcess', views.donationProcess, name='donationProcess'),
+    path('emergency', views.emergency, name='emergency'),
+    path('events', views.events, name='events'),
+    path('home', views.home, name='home'),
+
 ]
--- core/views.py
+++ core/views.py
@@ -3,3 +3,24 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def home(request):
+    return render(request, 'home.html')
+
+def donationCentres(request):
+    return render(request, 'donationCentres.html')
+
+def donationProcess(request):
+    return render(request, 'donationProcess.html')
+
+def events(request):
+    return render(request, 'events.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def bookingForm(request):
+    return render(request, 'bookingForm.html')
+
+def emergency(request):
+    return render(request, 'emergency.html')
